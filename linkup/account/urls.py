fda26d12186e89d85afb81a165750a9a74d358da
nargesrezaii <nargesrezaeiiiii@gmail.com>
2026-07-18 17:24:09 +0330

    feat: implement user registration

--- linkup/account/urls.py
+++ linkup/account/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
