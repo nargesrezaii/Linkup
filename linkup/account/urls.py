97720fc7fe0ef372d790e02767aee91f640abe12
nargesrezaii <nargesrezaeiiiii@gmail.com>
2026-07-18 18:12:57 +0330

    feat: enable profile editing

--- linkup/account/urls.py
+++ linkup/account/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
 
 if settings.DEBUG:
--- linkup/account/views.py
+++ linkup/account/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 from account.models import Profile
-from account.forms import LoginForm, UserRegistrationForm
+from account.forms import LoginForm, ProfileEditForm, UserEditForm, UserRegistrationForm
 
 def user_login(request):
     if request.method=='POST':
@@ -58,3 +58,21 @@ def register(request):
         'account/register.html',
         {'user_form': user_form}
     )
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        'account/edit.html',
+        {'user_form': user_form, 'profile_form': profile_form}
+    )
+        
