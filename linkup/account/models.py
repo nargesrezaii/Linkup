61285fc9064e1cdf3a978990b6c2f62575205d1e
nargesrezaii <nargesrezaeiiiii@gmail.com>
2026-07-20 17:20:01 +0330

    fix: enable profile photo uploads

--- linkup/account/models.py
+++ linkup/account/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
         upload_to='users/%Y/%m/%d',
         blank=True
     )
-    date_of_birth = models.DateField(blank=True, null=True)\
+    date_of_birth = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f'Profile of {self.user.username}'
